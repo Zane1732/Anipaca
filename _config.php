c7f77a23ccfc33cd1598a45554e5689d435c0731
Zane1732 <112121784+Zane1732@users.noreply.github.com>
2025-05-16 12:25:15 +0530

    Update _config.php

--- _config.php
+++ _config.php
@@ -1,6 +1,6 @@
 <?php 
 
-$conn = new mysqli("HOSTNAME", "USERNAME", "PASSWORD", "DATABASE");
+$conn = new mysqli("empty-sphynx-2956.internal", "root", "JzFsagqLlYIQgSiRIEaVzuvYXnihiFMU", "JzFsagqLlYIQgSiRIEaVzuvYXnihiFMU");
 
 if ($conn->connect_error) {
     error_log("Database connection failed: " . $conn->connect_error);
@@ -21,13 +21,13 @@ $telegram = "https://t.me/anipaca";
 $instagram = "https://www.instagram.com/pxr15_"; 
 
 // all the api you need
-$zpi = "https://your-hosted-api.com/api"; //https://github.com/PacaHat/zen-api
+$zpi = "https://q-iota-five.vercel.app/api"; //https://github.com/PacaHat/zen-api
 $proxy = $websiteUrl . "/src/ajax/proxy.php?url=";
+
 
 //If you want faster loading speed
 //$proxy = "https://your-hosted-proxy.com/api/v1/streamingProxy?url="; //https://github.com/MetaHat/m3u8-streaming-proxy
 
 
 $banner = $websiteUrl . "/public/images/banner.png";
-
     
